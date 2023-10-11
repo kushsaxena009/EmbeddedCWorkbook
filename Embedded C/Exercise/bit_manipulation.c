debfec82df191bedf025e0ec1f3e542c6c42f8e7
Kushagra Saxena <69414276+kushsaxena009@users.noreply.github.com>
2023-10-12 00:04:49 +0530

    Update bit_manipulation.c

--- Embedded C/Exercise/bit_manipulation.c
+++ Embedded C/Exercise/bit_manipulation.c
@@ -1,17 +1,12 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 int flag = 99;
 void menu(void);
 void compute_sign(int num);
 void check_opposite_signs(int a,int b);
+void check_powerOftwo(int num);
+void set_particularBit(int num,int bit);
+void clear_particularBit(int num,int bit);
 
 int main()
 {
@@ -28,9 +23,12 @@ void menu(void)
 {
     int val1, val2, opt = 0;
     printf("*************************************************\n");
+    printf("\tEnter 0 to exit application \n");
     printf("\tEnter 1 to check signed of number \n");
     printf("\tEnter 2 to check polarity of two numbers \n");
-    printf("\tEnter 0 to exit application \n\t_______________\n");
+    printf("\tEnter 3 to check power of two \n");
+    printf("\tEnter 4 to set bit of any number \n");
+    printf("\tEnter 5 to clear bit of any number \n");
     printf("*************************************************\n");
     scanf("%d",&opt);
     switch(opt)
@@ -48,6 +46,21 @@ void menu(void)
         printf("Please enter two numbers to check \n");
         scanf("%d %d",&val1, &val2);
         check_opposite_signs(val1, val2);
+        break;
+        case 3 :
+        printf("Please enter a number to check its power \n");
+        scanf("%d",&val1);
+        check_powerOftwo(val1);
+        break;
+        case 4 :
+        printf("Enter number to set particular bit and also enter set bit\n");
+        scanf("%d %d",&val1, &val2);
+        set_particularBit(val1,val2);
+        break;
+        case 5 :
+        printf("Enter number to clear particular bit and also enter set bit\n");
+        scanf("%d %d",&val1, &val2);
+        clear_particularBit(val1,val2);
         break;
         default:
         printf("Please enter valid option \n");
@@ -72,4 +85,47 @@ void check_opposite_signs(int a,int b)
     printf("Both numbers are different polarity\n");
     else
     printf("Both numbers are same polarity\n");
+}
+
+void check_powerOftwo(int num)
+{
+    int sizecheck;
+    sizecheck = sizeof(num);
+    for(int i=0;i<(sizecheck*8);i++)
+    {
+        if(num == 1<<i)
+        printf("Given number %d is power of two\n",num);
+        else
+        printf("Given number %d is not a power of two\n",num);
+    }
+}
+
+void set_particularBit(int num,int bit)
+{
+    int bit_check;
+    bit_check = (sizeof(num)*8);
+    if((bit >= bit_check) || (bit<0))
+    {
+        printf("Please enter valid bit size which is in range from 0 to %d\n",(bit_check-1));
+    }
+    else
+    {
+        num|= (1<<bit);
+        printf("Number after bit set is 0X%X\n",num);
+    }
+}
+
+void clear_particularBit(int num,int bit)
+{
+    int bit_check;
+    bit_check = (sizeof(num)*8);
+    if((bit >= bit_check) || (bit<0))
+    {
+        printf("Please enter valid bit size which is in range from 0 to %d\n",(bit_check-1));
+    }
+    else
+    {
+        num&= ~(1<<bit);
+        printf("Number after bit clear is 0X%X\n",num);
+    }
 }
